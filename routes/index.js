cc09585dd12826099c27f80857f4bbea24f42aed
bang-eric <ezraricardo@gmail.com>
2022-07-07 20:25:36 +0700

    push after solved continuous integration step

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var TestingController = require('../controllers/indexControllers')
 
+
 /* GET home page. */
 router.get('/', TestingController.testingGet);
 router.post('/login', TestingController.testingLoginFirebaseAuth);
